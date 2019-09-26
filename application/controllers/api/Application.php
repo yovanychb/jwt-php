6eb39442f7af64df4b713afa2a8bada80a781f22
Cesar Linares <celinares@hotmail.com>
2019-09-26 16:59:24 -0600

    cambios

--- application/controllers/api/Application.php
+++ application/controllers/api/Application.php
@@ -3,51 +3,37 @@ defined('BASEPATH') or exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-// use Restserver\Libraries\REST_Controller;
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 class Application extends REST_Controller
 {
     public function __construct()
     {
-        parent::__construct();
-
-        // Load these helper to create JWT tokens
+        parent::__construct('rest');
         $this->load->helper(['jwt', 'authorization']);
     }
 
     public function index_get()
     {
         $tokenData = 'Hello World!';
-
-        // Create a token
         $token = AUTHORIZATION::generateToken($tokenData);
-        // Set HTTP status code
         $status = parent::HTTP_OK;
-        // Prepare the response
         $response = ['status' => $status, 'token' => $token];
-        // REST_Controller provide this method to send responses
         $this->response($response, $status);
     }
 
     public function index_post()
     {
-        // Have dummy user details to check user credentials
-        // send via postman
-        $dummy_user = [
-            'username' => 'Test',
-            'password' => 'test'
+        $user = [
+            'username' => $this->post('username'),
+            'password' => $this->post('password')
         ];
-        // Extract user data from POST request
-        $username = $this->post('username');
-        $password = $this->post('password');
-        // Check if valid user
-        if ($username === $dummy_user['username'] && $password === $dummy_user['password']) {
+        $this->db->where('username', $this->post('username'));
+        $this->db->where('password', $this->post('password'));
+        $usuario = $this->db->get('usuarios')->row();
 
-            // Create a token from the user data and send it as reponse
-            $token = AUTHORIZATION::generateToken(['username' => $dummy_user['username']]);
-            // Prepare the response
+        if ($usuario != null) {
+            $token = AUTHORIZATION::generateToken(['username' => $user['username']]);
             $status = parent::HTTP_OK;
             $response = ['status' => $status, 'token' => $token];
             $this->response($response, $status);
@@ -58,27 +44,20 @@ class Application extends REST_Controller
 
     private function verify_request()
     {
-        // Get all the headers
         $headers = $this->input->request_headers();
-        // Extract the token
         $token = $headers['Authorization'];
-        // Use try-catch
-        // JWT library throws exception if the token is not valid
         try {
-            // Validate the token
-            // Successfull validation will return the decoded user data else returns false
             $data = AUTHORIZATION::validateToken($token);
             if ($data === false) {
                 $status = parent::HTTP_UNAUTHORIZED;
                 $response = ['status' => $status, 'msg' => 'Unauthorized Access!'];
                 $this->response($response, $status);
-                exit();
             } else {
-                return $data;
+                $status = parent::HTTP_OK;
+                $response = ['status' => $status, 'token' => $data];
+                $this->response($response, $status);
             }
         } catch (Exception $e) {
-            // Token is invalid
-            // Send the unathorized access message
             $status = parent::HTTP_UNAUTHORIZED;
             $response = ['status' => $status, 'msg' => 'Unauthorized Access! '];
             $this->response($response, $status);
@@ -87,11 +66,6 @@ class Application extends REST_Controller
 
     public function get_me_data_post()
     {
-        // Call the verification method and store the return value in the variable
         $data = $this->verify_request();
-        // Send the return data as reponse
-        $status = parent::HTTP_OK;
-        $response = ['status' => $status, 'data' => $data];
-        $this->response($response, $status);
     }
 }
